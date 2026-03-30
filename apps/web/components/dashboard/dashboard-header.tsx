056ba04d1bd084fb67ee13d7795495b10e00c64f
YuHong Cheung <3244878503@qq.com>
2026-03-30 21:00:14 +0800

    feat(theme): replace ModeToggle with AnimatedThemeToggler

--- apps/web/components/dashboard/dashboard-header.tsx
+++ apps/web/components/dashboard/dashboard-header.tsx
@@ -2,13 +2,14 @@
 
 import { SidebarTrigger } from "../ui/sidebar";
 import { ModeToggle } from "../mode-toggle";
+import { AnimatedThemeToggler } from "../ui/animated-theme-toggler";
 
 export default function DashboardHeader() {
   return (
     <header className="flex h-16 shrink-0 items-center gap-2">
       <div className="flex w-full items-center justify-between gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
-        <ModeToggle></ModeToggle>
+        <AnimatedThemeToggler></AnimatedThemeToggler>
       </div>
     </header>
   );
