14b07cdea11c9c38a4e13d7e2e9c9eb1c3b9b054
YuHong Cheung <3244878503@qq.com>
2026-04-13 20:22:53 +0800

    feat(login):  update login home  images and add theme toggle

--- apps/web/app/(home)/layout.tsx
+++ apps/web/app/(home)/layout.tsx
@@ -1,22 +1,34 @@
 import Link from "next/link";
 import { Logo } from "@eveheart/web-shared";
+import Image from "next/image";
+import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-        </div>
+        <div className="flex justify-center gap-2 md:justify-between">
           <Logo />
+          <AnimatedThemeToggler />
+        </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">{children}</div>
         </div>
       </div>
-      <div className="relative hidden bg-muted lg:block">
-        <img
-          src="/placeholder.jpg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+      <div className="relative hidden lg:block">
+        <Image
+          src="/login-dark.png"
+          alt="login page image"
+          fill
+          priority
+          className="object-cover"
+        />
+        <Image
+          src="/login.png"
+          alt="login page image"
+          fill
+          priority
+          className="object-cover dark:hidden"
         />
       </div>
     </div>
